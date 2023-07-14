111f0d92b61a55840b899e2880bd65ac2bbfb992
quduss <shuaibqudus33@gmail.com>
2023-07-14 17:55:14 +0100

    Write a c program that prints alphabets in lowercase then in lowercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A c program that prints the alphabet in lowercase, then in uppercase
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+	char b;
+
+	a = 'a';
+	b = 'A';
+	while (a <= 'z')
+	{
+		putchar(a++);
+	}
+	while (b <= 'Z')
+	{
+		putchar(b++);
+	}
+	putchar('\n');
+	return (0);
+}
