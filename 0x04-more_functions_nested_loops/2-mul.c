1ae20afe33d9b0f41851ff452d125b5ab7dc5fbc
quduss <shuaibqudus33@gmail.com>
2023-07-20 15:18:01 +0100

    Create a function that multiplies two integer inputs

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - multiplies two integer inputs
+ * @a: first integer
+ * @b: second integer
+ * Return: the product of the two integers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
