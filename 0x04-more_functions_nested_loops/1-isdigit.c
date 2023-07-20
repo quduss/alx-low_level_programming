7bea360dc2fa661b6c87782463f0cb99dba51a1f
quduss <shuaibqudus33@gmail.com>
2023-07-20 15:10:48 +0100

    Create a function that checks if input is a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+/**
+ * _isdigit - checks if input is a digit 0-9
+ * @c: integer input
+ * Return: 1 if c is a digit and 0 if not
+ */
+
+int _isdigit(int c)
+{
+	if ((c >= '0') && (c <= '9'))
+		return (1);
+	else
+		return (0);
+}
