db62693583091a3d0a122d4f6492cea240f939c9
quduss <shuaibqudus33@gmail.com>
2023-07-24 21:59:20 +0100

    Create a function that prints n elements of an array of ints

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,14 +9,17 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	i = 0;
-	while (i < n)
+	if (n > 0)
 	{
-		printf("%d", a[i]);
-		if (i != n - 1)
-			printf(", ");
-		else
-			printf("\n");
-		i++;
+		i = 0;
+		while (i < n)
+		{
+			printf("%d", a[i]);
+			if (i != n - 1)
+				printf(", ");
+			else
+				printf("\n");
+			i++;
+		}
 	}
 }
