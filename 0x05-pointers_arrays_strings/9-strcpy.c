9067b7507678bf31ed95bffe0279d44f1a8ab0bd
quduss <shuaibqudus33@gmail.com>
2023-07-24 22:58:58 +0100

    Create a function that copies src string to dest string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+/**
+ * _strcpy - copies the string pointed by source to dest
+ * @dest: dest string
+ * @src: source string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return dest;
+}
