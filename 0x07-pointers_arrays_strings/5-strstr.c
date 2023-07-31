a07fe459fe916bd80f57e8a8332a8afc5243db4a
quduss <shuaibqudus33@gmail.com>
2023-07-31 16:44:19 +0100

    Create a function that finds the first occurrence of needle string in haystack string

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include <stddef.h>
+/**
+ * _strstr - finds first occurrence of string needle in string
+ * haystack
+ * @haystack: haystack string
+ * @needle: needle string
+ * Return: pointer to first occurrence in haystack
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+			return (haystack + i);
+		}
+	}
+	return (NULL);
+}
