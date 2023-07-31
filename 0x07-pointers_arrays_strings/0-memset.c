677021d5d1e1f2550ccb424e6d04c9a38d6e3e53
quduss <shuaibqudus33@gmail.com>
2023-07-31 15:20:19 +0100

    Create a function that sets first n bytes of a memory area with a given value

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+/**
+ * _memset - fills memory area pointed by s with a constant byte
+ * @s: memory area to fill
+ * @b: constant byte to fill with
+ * @n: first number of bytes to fill
+ * Return: pointer to memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
