983589e711345591ec82d89321ed07213cdc86e9
quduss <shuaibqudus33@gmail.com>
2023-08-04 07:41:59 +0100

    Create a program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints the programs name
+ * @argc: argument count
+ * @argv: array of command line arguments
+ * Return: Always 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
