ce2c5cc71545f41db94498a3a362c8224d9aa513
quduss <shuaibqudus33@gmail.com>
2023-08-07 11:28:17 +0100

    Fix when size == 0 case

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,6 +12,8 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *s;
 
+	if (size == 0)
+		return (NULL);
 	s = malloc(sizeof(char) * size);
 	if (s == NULL)
 		return (NULL);
