a02fc8d5d0acb9c63291b98df392e1226fd38f18
qudys <shuaibqudus33@gmail.com>
2023-08-10 03:32:02 -0700

    Fix INT_MAX case

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,3 +1,4 @@
+#include <limits.h>
 #include <stdlib.h>
 /**
  * malloc_checked - allocates memory using malloc
@@ -9,6 +10,8 @@ void *malloc_checked(unsigned int b)
 {
 	void *ptr;
 
+	if (b == INT_MAX)
+		exit(98);
 	ptr = malloc(b);
 	if (ptr == NULL)
 		exit(98);
