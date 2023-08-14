3e41400160e36a067b4d4c775a6b4c75ffa041d5
quduss <shuaibqudus33@gmail.com>
2023-08-14 22:06:38 +0100

    Put definitions at the top

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,11 @@
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - type to describe the features of a typical dog
  * @name: name of dog
  * @age: age of a dog
  * @owner: owner of the dog
  */
-
-#ifndef DOG_H
-#define DOG_H
-
 struct dog
 {
 	char *name;
