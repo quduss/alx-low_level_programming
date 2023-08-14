871d1b1d68859ebe0bfcccd3b08d9679237dad3b
quduss <shuaibqudus33@gmail.com>
2023-08-14 21:48:24 +0100

    Create struct to describe a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+/**
+ * struct dog - type to describe the features of a typical dog
+ * @name: name of dog
+ * @age: age of a dog
+ * @owner: owner of the dog
+ */
+
+#ifndef DOG_H
+#define DOG_H
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
