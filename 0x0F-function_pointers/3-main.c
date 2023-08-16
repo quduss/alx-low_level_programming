1a18391e38377708ea0169dea03c6b2e47e9039d
quduss <shuaibqudus33@gmail.com>
2023-08-16 23:36:26 +0100

    Create a program that performs simple opeartions with two integer inputs

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * main - perform simple operations
+ * @argc: arguments count
+ * @argv: array of string arguments
+ * Return: 0 if no error
+ */
+
+int main(int argc, char *argv[])
+{
+	int (*a)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (!(argv[2][0] == '+' || argv[2][0] == '-' || argv[2][0] == '*' || argv[2][0] == '/' || argv[2][0] == '%'))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3][0] == '0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	a = get_op_func(argv[2]);
+	printf("%d\n", a(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+/**
+ * op_add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: addition of the two integers
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtracts second integer from first
+ * @a: first integer
+ * @b: second integer
+ * Return: result of the subtraction
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: product of the two integers
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides first integer by second
+ * @a: first integer
+ * @b: second integer
+ * Return: result of the division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - gets the remainder of a / b
+ * @a: first integer
+ * @b: second integer
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
