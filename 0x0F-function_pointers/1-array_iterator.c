0b61a36c18e6923bb2b3b45e7316e10bc5623757
qudys <shuaibqudus33@gmail.com>
2023-08-16 09:53:07 -0700

    Add "if array is null case"

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,7 +8,7 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (action != NULL && (size > 0))
+	if ((action != NULL) && (size > 0) && (array != NULL))
 	{
 		size_t i;
 
