936cc1f46036ea87ccee0a54bc4ec2e72d1c7451
quduss <shuaibqudus33@gmail.com>
2023-08-17 07:28:51 +0100

    Create a function that prints variable number of string arguments

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+/**
+ * print_strings - prints strings and separator in between
+ * @separator: pointer to separator
+ * @n: number of strings to print
+ * @...: variable string arguments
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	char *s;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(ap, char *);
+		if (s == NULL)
+			printf("nil");
+		if (s != NULL)
+			printf("%s", s);
+		if (i != (n - 1) && (separator != NULL))
+			printf("%s", separator);
+	}
+	va_end(ap);
+	printf("\n");
+}
