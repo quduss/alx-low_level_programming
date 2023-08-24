2dbdeb5510f31b9eab5c6805017fb0c57eb72849
quduss <shuaibqudus33@gmail.com>
2023-08-24 10:19:08 +0100

    Create a function that frees every node in a list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_list - frees every node in a list
+ * @head: pointer to the head node
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tempnode;
+
+	while (head->next != NULL)
+	{
+		tempnode = head->next;
+		free(head->str);
+		free(head);
+		head = tempnode;
+	}
+	free(head->str);
+	free(head);
+}
