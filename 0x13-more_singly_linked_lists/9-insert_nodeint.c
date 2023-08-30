1c951772d2c19c6b86241778298448b4d07f2cd6
quduss <shuaibqudus33@gmail.com>
2023-08-30 17:35:04 +0100

    Return NULL if malloc fails

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,6 +16,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
 	new_node->n = n;
 	if (idx == 0)
 	{
