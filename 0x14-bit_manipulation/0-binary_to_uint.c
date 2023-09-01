2fe491a24a8fe896e0284bf2abe9704051187ee1
quduss <shuaibqudus33@gmail.com>
2023-09-01 01:25:03 +0100

    Fix no description for _pow_recursion

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,22 @@
 #include "main.h"
 #include <stdlib.h>
+
+/**
+  * _pow_recursion - raise the power of x to y
+  * @x: x input
+  * @y: y input
+  * Return: x to the power y
+  */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, (y - 1)));
+}
+
 /**
  * binary_to_uint - converts a binary number in string format to
  * an unsigned int
@@ -28,19 +45,4 @@ unsigned int binary_to_uint(const char *b)
 		sum += (b[c] - 48) * _pow_recursion(2, i - 1 - c);
 	}
 	return (sum);
-}
- /**
-  * _pow_recursion - raise the power of x to y
-  * @x: x input
-  * @y: y input
-  * Return: x to the power y
-  */
-
-int _pow_recursion(int x, int y)
-{
-	if (y < 0)
-		return (-1);
-	if (y == 0)
-		return (1);
-	return (x * _pow_recursion(x, (y - 1)));
 }
