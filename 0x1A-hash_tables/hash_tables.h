3db49af659117182f84d83f6f59c37a3f3f9af85
quduss <shuaibqudus33@gmail.com>
2023-11-05 14:47:43 +0100

    Write a function that retrieves a value associated with a key.

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -36,4 +36,5 @@ unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 hash_node_t *add_hash_top(hash_table_t *ht, const char *key, const char *value);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
 #endif
