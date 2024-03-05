8c50a7402e2a7cb24acbdb8d8e995659b7fd05c8
quduss <shuaibqudus33@gmail.com>
2024-03-05 20:56:22 +0100

    Close fd if buf is NULL

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,17 +12,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	void *buf;
-	ssize_t r;
+	ssize_t r, w;
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	buf = malloc(letters);
 	if (buf == NULL)
+	{
+		close(fd);
 		return (0);
+	}
 	r = read(fd, buf, letters);
 	close(fd);
-	write(1, buf, r);
+	w = write(1, buf, r);
 	free(buf);
 	return (r);
 }
