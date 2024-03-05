34c034fd9e6a4e9278737d6a96b57d7ac5f1d32f
quduss <shuaibqudus33@gmail.com>
2024-03-05 21:18:16 +0100

    Return 0 if read or write fails

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,7 +12,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	void *buf;
-	ssize_t r;
+	ssize_t r, w;
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
@@ -24,8 +24,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	r = read(fd, buf, letters);
+	if (r == -1)
+	{
+		close(fd);
+		return (0);
+	}
 	close(fd);
-	write(1, buf, r);
+	w = write(1, buf, r);
+	if (w == -1)
+	{
+		free(buf);
+		return (r);
+	}
 	free(buf);
 	return (r);
 }
