3480d0cadd2f80f69222b19fc1a8376c3d6dbcbb
quduss <shuaibqudus33@gmail.com>
2024-03-15 13:17:56 +0100

    Initialise hash table on a separate line

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,9 +7,10 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht = malloc(sizeof(hash_table_t));
+	hash_table_t *ht;
 	unsigned long int i;
 
+	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
 	ht->size = size;
