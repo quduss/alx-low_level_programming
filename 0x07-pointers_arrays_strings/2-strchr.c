0f088d33565c844f138a981e9b7c8d5e064d3c66
quduss <shuaibqudus33@gmail.com>
2024-06-22 14:29:44 +0100

    Fix a case where '\0' is passed as an argument

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,10 +10,16 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	i = 0;
+	while (1)
 	{
+		if (s[i] == '\0')
+			break;
 		if (s[i] == c)
 			return (s + i);
+		i++;
 	}
+	if (s[i] == c)
+		return (s + i);
 	return (NULL);
 }
